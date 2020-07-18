d22ad098efa8dbd2a5f4fa95ced226bc7bae4a3f
Singh <singh2rajiv@dgb-c02vh08bh.lan>
2020-07-18 10:41:18 -0700

    Day 14 - Value of Pie using Monte Carlo method

---
+++ src/Day14ValueOfPie.java
@@ -0,0 +1,40 @@
+/*
+ * The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
+ * 
+ * Hint: The basic equation of a circle is x2 + y2 = r2.
+ */
+
+public class Day14ValueOfPie {
+
+	public static void main(String[] args) {
+		
+		System.out.println(getPi(10000));
+		System.out.println(getPi(100000));
+		System.out.println(getPi(1000000));
+		System.out.println(getPi(10000000));
+
+	}
+	
+	public static double getPi(int numThrows){
+		
+		int inCircle= 0;
+		
+		for(int i = 0; i < numThrows; i++){
+			
+			//a square with a side of length 2 centered at 0 has 
+			//x and y range of -1 to 1
+			double randX= (Math.random() * 2) - 1;//range -1 to 1
+			double randY= (Math.random() * 2) - 1;//range -1 to 1
+			
+			//distance from (0,0) = sqrt((x-0)^2+(y-0)^2)
+			double dist= Math.sqrt(randX * randX + randY * randY);
+			
+			//^ or in Java 1.5+: double dist= Math.hypot(randX, randY);
+			if(dist < 1){//circle with diameter of 2 has radius of 1
+				inCircle++;
+			}
+		}
+		return 4.0 * inCircle / numThrows;
+	}
+
+}
