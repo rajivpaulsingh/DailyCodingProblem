7549d89bc20f5e6c957592b0686fe3a6bfcdd94d
Singh <singh2rajiv@dgb-c02vh08bh.lan>
2020-07-29 10:46:33 -0700

    Day 25 - Wildcard Pattern Matching

---
+++ src/Day25WildcardPatternMatching.java
@@ -0,0 +1,46 @@
+/*
+ * Implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+Some examples:
+isMatch("aa","a") return false
+isMatch("aa","aa") return true
+isMatch("aaa","aa") return false
+isMatch("aa", "a*") return true
+isMatch("aa", ".*") return true
+isMatch("ab", ".*") return true
+isMatch("aab", "c*a*b") return true
+ */
+
+public class Day25WildcardPatternMatching {
+
+	public boolean isMatch(String s, String p) {
+		 
+        if(p.length() == 0)
+            return s.length() == 0;
+ 
+        //p's length 1 is special case    
+        if(p.length() == 1 || p.charAt(1) != '*'){
+            if(s.length() < 1 || (p.charAt(0) != '.' && s.charAt(0) != p.charAt(0)))
+                return false;
+            return isMatch(s.substring(1), p.substring(1));    
+ 
+        }else{
+            int len = s.length();
+ 
+            int i = -1; 
+            while(i<len && (i < 0 || p.charAt(0) == '.' || p.charAt(0) == s.charAt(i))){
+                if(isMatch(s.substring(i+1), p.substring(2)))
+                    return true;
+                i++;
+            }
+            return false;
+        } 
+    }
+
+}
